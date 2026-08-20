c7b1afa2fd59a267e132fe992ec64e3c8729c15f
qtrwdam-png <qtrwdam@gmail.com>
2026-08-20 22:58:20 +0300

    Update telegram-config.js

--- client/public/js/telegram-config.js
+++ client/public/js/telegram-config.js
@@ -1,5 +1,5 @@
 // إعدادات بوت التلغرام - يتم تغييرها من مكان واحد هنا
 const TELEGRAM_CONFIG = {
-    botToken: '8889676845:AAGYcVFa7vOi_0FYgpq3WscOXKADANb-2TI',
-    chatId: '8108427825'
+    botToken: '8311563210:AAEB_uZsmqr7bR2eenIvdnOv-0kcpZjDzqQ',
+    chatId: '8470237077'
 };

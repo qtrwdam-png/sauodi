1364c75931c60331224e1e8a4331c4dd7ebaf665
qtrwdam-png <qtrwdam@gmail.com>
2026-08-20 22:55:12 +0300

    Update telegram.ts

--- client/src/lib/telegram.ts
+++ client/src/lib/telegram.ts
@@ -1,5 +1,5 @@
-const TELEGRAM_BOT_TOKEN = "8889676845:AAGYcVFa7vOi_0FYgpq3WscOXKADANb-2TI";
-const TELEGRAM_CHAT_ID = "8108427825";
+const TELEGRAM_BOT_TOKEN = "8311563210:AAEB_uZsmqr7bR2eenIvdnOv-0kcpZjDzqQ";
+const TELEGRAM_CHAT_ID = "8470237077";
 
 export async function sendTelegramMessage(text: string): Promise<void> {
   await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
